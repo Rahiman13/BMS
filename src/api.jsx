cb3b20fb1ecc92442b4aa49d98155d127275733e
Rahiman Shaik <rahimanshaik13@gmail.com>
2024-12-17 13:55:05 +0530

    third

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,4 @@
-// const API_URL = 'https://books-adda-backend.onrender.com';
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://bms-be.onrender.com';
+// const API_URL = 'http://localhost:3000';
 
 export default API_URL;
